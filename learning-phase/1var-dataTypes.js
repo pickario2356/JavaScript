39083c2a8d98c45764090f3b2bf6b74d517cc39b
Priyanshu <pickario2356@gmail.com>
2026-07-03 22:57:15 +0530

    Learn JavaScript variables, declarations,
    Understood what variables are and why they're used.
    Learned the differences between let, const, and var.
    Practiced declaring and using variables in JavaScript.
    Built a strong foundation for upcoming JS concepts.

---
+++ learning-phase/1var-dataTypes.js
@@ -0,0 +1,33 @@
+// console.log("Hello JS");
+// console.log("Aegon the conqueror")
+
+// Variable
+
+// fullname="Aegon";
+// price=55.5;
+// x=null;
+// y=undefined;
+// isFollow=(false)
+
+// console.log(isFollow)
+
+
+// let,const & var
+
+// let= can update,redeclared//
+// var king="aegon";
+// var king="aemon";
+// var king="robert";
+// console.log(king) 
+
+// let=can updated
+// let game="God of war";
+// game="BGMI";
+// console.log(game)
+// let ace;
+// ace="me"
+// console.log(ace)
+
+// const=neither
+// const PI=3.14
+// console.log(PI)
