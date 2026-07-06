dc4482bc04d291e74594fb824645396b2cb19d33
Priyanshu <pickario2356@gmail.com>
2026-07-06 23:24:22 +0530

    learn string methods and basic loop practice
    learn string methods and basic loop practice

---
+++ learning-phase/3string.js
@@ -0,0 +1,38 @@
+// Strings are ""IMMUTABLE"""
+// let str = "Pickario";
+// console.log(str[4])
+// console.log(str.length)
+
+
+// template literals
+// let sentence =`This is a template literals`;
+// console.log(sentence)
+
+
+// const obj={
+//     item:"levithan",
+//     price:5,
+// };
+// // console.log("this item called:",obj.item,"is of:",obj.price);
+// let output = `this item is \t called ${obj.item} is of\n${obj.price} or ${5+5}`
+// console.log(output)
+// escape character jese \t ya \n ek hi legth mae jate hai do nhi
+
+
+
+// METHODS
+
+// this method does not chage the main str but make a new one
+let str1 = "aegon"
+let str2 = "   The   Egg"
+let str3 = '12345689'
+
+// console.log(str1.toUpperCase());
+// console.log(str2.toLowerCase());
+// console.log(str2.trim());
+// console.log(str3.slice(2,5));
+// console.log(str1.concat(str2));
+// console.log(str1.replace("gon","don"));
+console.log(str2.charAt(3))
+
+
--- practice/practice3.js
+++ practice/practice3.js
@@ -16,12 +16,52 @@
 //     }
 // }
 
-let gameNum = 67;
-let num = prompt("guess the number")
+// let gameNum = 67;
+// let num = prompt("guess the number")
+// while(num!=gameNum){
+//     num = prompt("guess again")
+// }
+// // yaha != ka mtlb same nahi toh loop chalo aur agr == krta jo glt hota to wo keta same aye to loop chalao
+// console.log("congratulation u guessd the num")
 
-while(num!=gameNum){
-    num = prompt("guess again")
-}
-// yaha != ka mtlb same nahi toh loop chalo aur agr == krta jo glt hota to wo keta same aye to loop chalao
+// let i=1;
+// let sum=0;
+// for(i;i<=5;i++){
+//     sum=sum+i;
+    
+// }
+// console.log(sum)
 
-console.log("congratulation u guessd the num")
+// for(i=1;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// let sum = 0;
+// for(i=1;i<=100;i++){
+//     if(i %3 ==0){
+//         sum =sum+i
+//     }
+// }
+// console.log(sum)
+
+// let sum=0;
+// for(i=1;i<=100;i++){
+//     if(i %6==0 ){
+//         sum=sum+i
+//     }
+// }
+// console.log(sum)
+
+// let sum=0;
+// for(i=0;i<=100;i++){
+//     if(i%2!=0){
+//     sum=sum+i}
+// }
+// console.log(sum)
+
+
+let fullname = prompt("enter the username");
+let username = "@"+fullname+fullname.length;
+console.log(username)
