102a0ed0b9f22ad56ea838271811f3c5bf711eb9
Priyanshu <pickario2356@gmail.com>
2026-07-07 23:39:07 +0530

    understand JavaScript arrays and basic operations
    Learned what arrays are and why they are used
    Explored array indexing and element access
    Understood how to store multiple values in a single variable
    Built a strong foundation for working with collections in JavaScript

---
+++ learning-phase/4Arrays.js
@@ -0,0 +1,61 @@
+// property just givs the value and methods do the work
+
+// let marks = [84,67,84,66,9,75];
+
+// marks[1] = 20
+// console.log(marks);
+// console.log(marks[2])
+
+
+// ""FOR""
+// let char = ["ichigo","aizen","urahara","levi","harald","imu"]
+
+// for (let indx=0;indx<char.length;indx++){
+//     console.log(char[indx])
+// }
+
+
+// ""FOR OF""
+// for(let heroes of char){
+//     console.log(heroes)
+// }
+
+let agent =["jin","kazyua","jin-pachi","jake","robo","ned"];
+let demon =["muzan","akaza","ichigo"]
+
+// console.log(agent)
+// agent.push("gon","king");
+// console.log(agent)
+
+// console.log(agent)
+// agent.unshift("gon","king");
+// console.log(agent)
+
+// console.log(agent)
+// console.log(agent.pop())
+
+// console.log("deleted",demon.shift())
+// demon.shift()
+// console.log(demon)
+
+// console.log(agent.toString())
+
+// console.log(agent.concat(demon))
+
+// console.log(agent)
+// console.log(agent.slice(1,3)) // not make change in orgnl
+
+let arr = [1,2,3,4,5,6,7,8]
+
+// arr.splice(2,2,900,420)// make change in orgnl
+// console.log(arr)
+
+// arr.splice(2,0,900,420)// add
+// console.log(arr)
+
+// arr.splice(2,2)// delete
+// console.log(arr)
+
+// arr.splice(2,1,900)// replace
+// console.log(arr)
+
---
+++ practice/practice4.js
@@ -0,0 +1,49 @@
+// let marks = [85,97,44,37,76,60] 
+// let sum = 0
+
+// for (i=0;i<marks.length;i++){
+//     console.log(marks[i]);
+//     sum=sum+marks[i]
+// }
+// console.log(sum)
+// let avg = sum/marks.length
+// console.log(`your avg is ${avg}`)
+
+// for(let i of marks){
+//     sum+=i
+// }
+// console.log(sum);
+
+
+// let item =[250,645,300,900,50]
+
+// let i=0
+// for (let val of item){
+//     console.log(`Original price is: ${val}`);
+//     let offer = item[i]/10;
+//     let discount=item[i]-offer
+//     console.log(`Discount price is: ${discount}`);
+//     i++;
+
+// }
+
+// for(let i = 0;i<item.length;i++){
+//     console.log(`Original price is: ${item[i]}`);
+//     let offer=item[i]/10;
+//     let discount=item[i]-offer;
+//     console.log(`discount price is: ${discount}`);
+    
+// }
+
+
+
+// let companies = ["TCS","AMAZON","uber","Google","IBM"]
+
+// companies.shift()
+// console.log(companies)
+
+// companies.splice(2,1,"ola")
+// console.log(companies)
+
+// companies.push("Flipkart")
+// console.log(companies)
