70700459e7c7dd6575bbfda825ca26fc88f0bd77
Priyanshu <pickario2356@gmail.com>
2026-07-08 21:04:02 +0530

    learn functions, parameters, arguments and return basics
    - Learned how to declare and call functions
    - Understood the difference between parameters and arguments
    - Explored function execution flow
    - Practiced console.log() vs return
    - Understood why functions without return give undefined
    - Built simple function examples using arithmetic operations

---
+++ learning-phase/5function.js
@@ -0,0 +1,43 @@
+// function myFunction(){
+//     console.log("Beyblade Burst")
+//     console.log("Beyblade Burst")
+//     console.log("Beyblade Burst")
+// }
+// myFunction()
+
+
+// function greet(name){       // name yaha "parameter"jo ek trh sai input hai
+//     console.log("hello",name)
+// }
+
+// greet("ishant")             // greet yaha paameter
+
+
+// function add(x,y){   // here it is not returnable
+//     console.log(x+y)
+// }
+// add(43,91)
+
+// function add(x,y){//ar ye parameter localvariable hote hai-->blokscope
+//     s = x+y;
+//     return s;
+// }
+
+// let sum = add(43,91)
+// console.log(sum)
+
+
+// ARROW FUNCTION
+
+// here sum is bth variable and functn
+
+// const sum = (a,b) => {
+//     console.log(a+b)
+// }
+// sum(41,2)
+
+// const sum = (a,b) => {
+//     return a+b
+// } 
+// let add=sum(41,2)
+// console.log(add)
