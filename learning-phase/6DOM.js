ee393d351ece870ed456052c59ea9c3699ef9ba3
Priyanshu <pickario2356@gmail.com>
2026-07-10 22:57:18 +0530

    Explore DOM properties: tagName, innerText, innerHTML and child nodes
    Learned the fundamentals of the DOM by exploring element selection with querySelector(), understanding DOM properties like tagName, innerText, and innerHTML, comparing firstChild vs firstElementChild, and understanding the difference between DOM elements and text values. Also practiced reading and manipulating content using JavaScript.

---
+++ learning-phase/6DOM.js
@@ -0,0 +1,55 @@
+// "doucument object model "(DOM)
+
+
+// window.console.log("yo")
+// console.log(window.document)
+// console.dir(window.document)// For object
+
+
+// document.body.style.background="white"
+
+// let heading = document.getElementsByClassName("myheading")
+// console.dir(heading)
+
+// let button = document.getElementById("mybutton")
+// console.dir(button)
+// console.log(button)
+
+// let para = document.getElementsByTagName("p")
+// console.dir(para)
+
+
+// "querySelector"
+
+// let firstEl = document.querySelector(".myheading")
+// console.dir(firstEl)
+
+
+// let allEl = document.querySelectorAll(".myheading")
+// console.dir(allEl)
+
+
+// let firstEl = document.querySelector("#mybutton")
+// console.dir(firstEl)
+
+
+// let firstEl = document.querySelector("p")
+// console.dir(firstEl)
+
+
+// "PROPERTIES"
+
+// console.dir(document.querySelector("p").tagName)
+// console.dir(firstEl.tagName)
+
+
+// console.dir(document.querySelector("div").innerHTML)
+
+let text = document.querySelector("div").innerText;
+
+console.dir(text)
+
+
+
+
+
