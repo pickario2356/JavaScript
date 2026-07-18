c0faccc82797a9e473ad7a35db57d3b3dceca4ec
Priyanshu <pickario2356@gmail.com>
2026-07-18 22:53:07 +0530

    Learned JavaScript Classes and Inheritance
    Studied JavaScript OOP fundamentals including classes, objects, constructors, methods, this, object creation with new, inheritance using extends, and parent access with super(). Cleared the difference between properties vs methods and understood when () are required.

--- learning-phase/11classes.js
+++ learning-phase/11classes.js
@@ -17,29 +17,110 @@
 // prototype are pre build fnx  
 // bhalu.__proto__ = employee// means employe ki fnx bhalu mai bhi in prototype
 
-// classes
 
 
-class Car{// Function
-    constructor(tire){// ye sidha kaam krta hai mtlb sbse pehle
-        console.log("Yo me Constructor")
-    }
-    up(){
-        console.log("our compay is going up")
-    }
-    down(){
-        console.log("Our company is going Downn")
-    }
+// "CLASSES"
 
-    setProduct(product){
-        this.productName=product;// here this.(property) hoti hia
-        this.price="unlimited"
-    }
-}
+// class Car{// Function
+//     constructor(tire,milage){// automatically and sbse pehle
+//         console.log("Yo me Constructor");
+//         this.brand = tire
+//         this.milage= milage
+//     }
+//     up(){
+//         console.log("Start")
+//     }
+//     down(){
+//         console.log("stop")
+//     }
 
-
-let Buggati = new Car("golden");// Object
-let Auddi = new Car();
-Auddi.setProduct("wheel")// isse setproduct ka default variable change hor aai
-
+//     // setProduct(product){
+//     //     this.productName=product;// here this.(property) hoti hia
+//     //     this.price="unlimited"
+//     // }
+// }
+// let Buggati = new Car("Royal Brand",67);// Object
+// console.log(Buggati)
+// let Auddi = new Car("idk",10);
+// console.log(Auddi)
+// Auddi.brand="Cureently Cheapest"// in CL yaha this ki no need direct property name
+// Auddi.setProduct("wheel")// isse setproduct ka default variable change hor aai
 // console.log(Amazon.setProduct("money"))// undefined cuz no return
+
+
+
+// "INHERITANCE"
+
+// class parent {
+//     hello(){
+//         console.log("Helooooooooo")
+//     }
+// }
+
+// class child extends parent {}
+
+// let milk = new child()
+// console.log(milk.hello())// () sai fnx work hota hai agr nahi () lgaya to jo gnx hai wo print hoga
+
+
+// class  Human{
+//     constructor(){
+//         this.species="Celestialsapien"
+//     }
+//     eat(){
+//         conosle.log("I can eat anything")
+//     }
+//     game(){
+//         console.log("I AM MASTER IN ALL GAMES")
+//     }
+//     work(){
+//         console.log("unemployed")
+//     }
+// }
+
+// class codder extends Human{
+//     work(){
+//         console.log("I TRAIN AI MODELS")
+//     }
+// }
+
+// let AlienX= new Human
+// // console.log(AlienX.constructor())// ye glt cuz constructor esa special fnx hai jo automaticaly hota hai to call nahi krna pdta to ()  ki bhi no need
+// console.log(AlienX.species)
+// // isme () nahi cuz species ek fnx nahi ek property(variable) hai 
+// let Aegon = new codder()
+// Aegon direct console sai fnx print hoga aAegon.xyz() sai fnx work krta hai
+// if same fnx dono mai hai to child wale superior hai
+// console.log(Aegon.game())// will work
+
+
+// "SUPER KEYWORD"
+
+// class  Human{
+//     constructor(name){
+//         this.species="Celestialsapien"
+//         this.name = name
+//         // console.log("enter parent constructor")
+//     }
+//     eat(){
+//         console.log("I can eat anything")
+//     }
+// }
+// class codder extends Human{
+//     // constructor(branch){
+//      constructor(name){
+//         // console.log("enter child constructor")
+//         // super()// parent kai fnx ko acces kai liye ya keh sakte hai parent mai add krne kai liyer
+//         // this.branch = branch
+//         // console.log("exit child constructor")
+
+//         super(name)// spr aur cnstrct dono mai name cuz super parent sai acces cnstrct kia accese ho ra hai ye tell
+//     }
+//     work(){
+//         super.eat()
+//         console.log("I TRAIN AI MODELS")
+//     }
+// }
+
+// let coddObj = new codder("Priyanshu")
+// // console.log(coddObj.work())
