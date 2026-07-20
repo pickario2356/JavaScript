90e1f563a663518ba2f72ce61296c328bba86981
Priyanshu <pickario2356@gmail.com>
2026-07-20 23:28:30 +0530

    Learned JavaScript Promises deeply: understood Promise creation, return new Promise(), pending/fulfilled states, resolve vs reject, .then()/.catch(), callback flow, function references vs function calls, and how returned Promises are stored and chained for asynchronous operations.
    Today's session was less about writing code and more about fixing my mental model of Promises. I spent time understanding why functions are passed into Promise constructors, how `return` connects a function to the calling variable, where `res` in `.then()` comes from, and the difference between callbacks, parameters, arguments, and returned values. It felt slow, but building this foundation now will make async/await much easier later.

---
+++ learning-phase/12callbacks2.js
@@ -0,0 +1,115 @@
+// function asyncFnx1(){
+//     return new Promise((resolve, reject)=>{
+//       setTimeout(()=>{
+//         console.log("Data1");
+//         resolve("completed1")
+//       },3000)
+//     })
+// }
+// function asyncFnx2(){
+//     return new Promise((resolve, reject)=>{
+//       setTimeout(()=>{
+//         console.log("Data2");
+//         resolve("completed2")
+//       },3000)
+//     })
+// }
+// console.log("Data is Fetching.......")
+// asyncFnx1().then((res)=>{
+//     console.log(res);
+
+// console.log("Data is Fetching.......")
+// asyncFnx2().then((res)=>{
+//     console.log(res);
+// })
+// })
+
+
+
+
+// function getData(dataId){
+//     return new Promise((resolve,reject)=>{//we get pending until we ad resolve or rejectfnx
+//         setTimeout(()=>{
+//         console.log("data:",dataId)
+//         resolve("done")
+//     },2000)
+//     })
+// }
+// getData(1).then((res)=>{
+//     console.log(res);
+//     getData(2).then((res)=>{
+//         console.log(res)
+//     })
+// })
+
+// Better
+// getData(1).then((res)=>{
+//     console.log(res)
+//     return getData(2)
+// }).then((res)=>{
+//     console.log(res)
+//     return getData(3)
+// }).then((res)=>{
+//     console.log(res)
+// })
+// Return Fnx sai bahr value use krne kai liye hoti hai jese yaha
+// getData(1).then((res)=>{
+//     console.log(res)
+//     return getData(2)
+// }) pai fnx khtm but get data @ kai liye return
+
+
+// Async Await
+
+// function api(){
+//     setTimeout(()=>{
+//         return new Promise((res,re)=>{
+//             console.log("hehehe hahah")
+//             res(200)
+//         })
+//     },2000)
+// }
+// async function getWeather(){
+//     await api()
+// }
+// console.log(getWeather())
+
+
+// function getData(dataId){
+//     return new Promise((resolve,reject)=>{//we get pending until we ad resolve or rejectfnx
+//         setTimeout(()=>{
+//         console.log("data:",dataId)
+//         resolve("done")
+//     },2000)
+//     })
+// }
+// // async function gettingData(){
+// //     await getData(1)
+// //     console.log("GettingData1")
+// //     await getData(2)
+// //     console.log("GettingData2")
+// //     await getData(3)
+// //     console.log("GettingData3")
+// // }
+// (async function (){
+//     await getData(1)
+//     console.log("GettingData1.........")
+//     await getData(2)
+//     console.log("GettingData2.........")
+//     await getData(3)
+//     console.log("GettingData3.........")
+// })();
+
+
+
+// different;
+// let a=6;
+// let b=7;
+// console.log(a+b)
+// try {console.log(a-x)}
+// catch(err){
+//     console.log(err)
+// }
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a^b)
