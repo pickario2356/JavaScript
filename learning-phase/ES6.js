e285520c728469eeecc8775ebc925694a684dc7a
Priyanshu <pickario2356@gmail.com>
2026-07-27 23:20:52 +0530

    learning ES6 for React JS

---
+++ learning-phase/ES6.js
@@ -0,0 +1,58 @@
+// Var,Let,Const
+// var=>for Whole fnx
+// let=>block(like here in loop)
+// const=>same as let but it can not be updated
+
+
+// function sayhehe(){
+    // for(var i=0;i<5;i++){
+
+    // for(let i=0;i<5;i++){
+    //     console.log(i)
+    // }
+    // console.log(i)
+
+    // const yoo= 5+6
+    // yoo=2
+    // console.log(yoo)
+// }
+// sayhehe()
+
+
+// objects
+
+// const person = {
+//     name:"blah blah",
+//     action:function fighting(){ let num = 55;return num},// action is method "figting" is fnx name
+//     move(){}// move is method in prsn obj
+// }
+// // to call
+
+// console.log(person.action())
+
+// // const target ='action()'    // function CALL
+// const target ='action'         // function ko GET karo
+// console.log(person[target]())  // pehle function nikalo, fir CALL karo
+
+
+// arrow Fnx
+
+// Function Declaration
+// function greet() {}
+
+// Function Expression
+// const greet = function () {}
+
+// basic fat arrow fnx
+// var a =()=>{}
+
+// fat arrow fnc with one parameter
+// var b = param=>{}
+// b(12)// ek value mai fat"()" hata sakte hai do mai fat jruri
+
+// fat arrow fn with implicit return
+// var g = ()=>123
+// console.log(g())
+
+
+// template literals backtick
